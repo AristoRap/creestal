c82c4571d5dd6b9a16beee6521c9b189a4a86cef
Aristotelis Rapai <aristotelisrapai@gmail.com>
2026-05-04 03:51:39 +0200

    fix command aliases spec

--- src/creestal.cr
+++ src/creestal.cr
@@ -6,7 +6,7 @@ require "./cli"
 
 # TODO: Write documentation for `Creestal`
 module Creestal
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 
   def self.run(argv : Array(String) = ARGV)
     CLI.run(argv)
